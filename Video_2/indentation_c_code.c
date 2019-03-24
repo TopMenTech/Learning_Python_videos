5bb57e28fe60942cd2e08d0374122875e59970b2
TopMenTech <48127176+TopMenTech@users.noreply.github.com>
2019-03-24 18:02:04 -0500

    Update indentation_c_code.c
    Demonstrate whitespace doesn't matter

--- Video_2/indentation_c_code.c
+++ Video_2/indentation_c_code.c
@@ -2,15 +2,4 @@
 #include <math.h>
 
 int main()
-{
-    double base, exponent, result;
-
-    base = 2.0;
-    exponent = 4.0;
-
-    result = pow(base, exponent);
-
-    printf("%.2lf", result);
-
-    return 0;
-}
+{double base, exponent, result; base = 2.0; exponent = 4.0; result = pow(base, exponent); printf("%.2lf", result); return 0;}
